2d8d0ab6891bf793f18334bf453dfaf4070c0b81
Galvas <galvaomercante@gmail.com>
2025-07-24 14:49:02 -0300

    Create: created home page

--- ContactControl/Controllers/HomeController.cs
+++ ContactControl/Controllers/HomeController.cs
@@ -6,15 +6,10 @@ namespace ContactControl.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+   
         public IActionResult Index()
         {
+           
             return View();
         }
 
